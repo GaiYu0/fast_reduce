d7c2da65e83441931fbf05c42a23741b82f30915
Yu Gai yu_gai@berkeley.edu <yu@pinwheel4.d2.comp.nus.edu.sg>
2020-03-05 15:37:27 +0800

    sync

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='min2d_2nd_dim',
+    name='pytorch_minimax',
     ext_modules=[
-        CUDAExtension('min2d_2nd_dim_cuda', [
-            'min2d_2nd_dim_cuda.cpp',
-            'min2d_2nd_dim_cuda_kernel.cu',
+        CUDAExtension('pytorch_minimax', [
+            'minimax_cuda.cpp',
+            'minimax_cuda_kernel.cu',
         ])
     ],
     cmdclass={
